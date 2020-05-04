9686e5e718d620f8f35898be7a740f89591752e9
sinahernandez <herjaredsinai@gmail.com>
2020-05-04 11:23:56 -0400

    Add collision functionality and player movement

--- src/game.py
+++ src/game.py
@@ -2,10 +2,11 @@ from cocos.layer import ScrollingManager, ScrollableLayer, Layer
 from cocos.sprite import Sprite
 from cocos.scene import Scene
 from cocos.tiles import load
+from cocos.director import director
+from cocos.actions import Move
+from cocos import mapcolliders
 
 from pyglet.window import key
-
-from math import cos, sin, radians, fabs
 
 
 class Background:
@@ -18,79 +19,48 @@ class Background:
         self.layer2 = bg["ground"]
         self.layer3 = bg["walls"]
         self.layer4 = bg["obstacle"]
+        self.colliders = bg["colliders"]
 
 
-class Player_Ship_Layer(ScrollableLayer):
+class Player_Layer(ScrollableLayer):
 
-    def __init__(self):
-        super(Player_Ship_Layer, self).__init__()
+    def __init__(self,collision_handler):
+        super(Player_Layer, self).__init__()
 
         self.ship = Sprite('elf.png')
         self.add(self.ship)
 
-
         self.ship.x = 100
         self.ship.y = 100
 
-        self.ship.x_velocity = 0
-        self.ship.y_velocity = 0
+        self.ship.velocity = (0,0)
+        self.ship.collide_map = collision_handler
+        self.ship.do(Mover())
 
-        self.ship.acceleration_rate = 1/10
+class Mover(Move):
+    # step() is called every frame.
+    # dt is the number of seconds elapsed since the last call.
+    def step(self, dt):
+        if dt > 0.1:
+            return
 
+        # Determine velocity based on keyboard inputs.
+        velocity_x = 75 * (keyboard[key.RIGHT] - keyboard[key.LEFT])
+        velocity_y = 75 * (keyboard[key.UP] - keyboard[key.DOWN])
 
-        self.ship.rotation = 0
-        self.ship.rotational_velocity = 0
+        dx = velocity_x * dt
+        dy = velocity_y*dt
 
-        self.ship.rotational_acceleration_rate = 0
+        last = self.target.get_rect()
 
+        new = last.copy()
+        new.x += dx
+        new.y += dy
 
-        self.ship.max_speed = 20
-        self.ship.max_rotational_speed = 5
+        # Set the object's velocity.
+        self.target.velocity = self.target.collide_map(last, new, velocity_x,velocity_y)
 
-
-    def handle_key_input(self, keys):
-
-        # Accelerate rotation counter-clockwise.
-        if keys[key.symbol_string(key.A)]:
-            self.ship.rotational_velocity -= self.ship.rotational_acceleration_rate
-
-        # Accelerate rotation clockwise.
-        if keys[key.symbol_string(key.D)]:
-            self.ship.rotational_velocity += self.ship.rotational_acceleration_rate
-
-        # Accelerate forward.
-        if keys[key.symbol_string(key.W)]:
-            self.ship.x_velocity += cos(radians(self.ship.rotation)) * self.ship.acceleration_rate
-            self.ship.y_velocity -= sin(radians(self.ship.rotation)) * self.ship.acceleration_rate
-
-        # Accelerate backward.
-        if keys[key.symbol_string(key.S)]:
-            self.ship.x_velocity -= cos(radians(self.ship.rotation)) * self.ship.acceleration_rate
-            self.ship.y_velocity += sin(radians(self.ship.rotation)) * self.ship.acceleration_rate
-
-        # Set both translational and rotational velocities to 0.
-        if keys[key.symbol_string(key.SPACE)]:
-            self.ship.x_velocity, self.ship.y_velocity, self.ship.rotational_velocity = 0, 0, 0
-
-        # Set position and rotation to 0.
-        if keys[key.symbol_string(key.R)]:
-            self.ship.x, self.ship.y, self.ship.rotation = 0, 0, 0
-
-
-    def update_ship_spacial_properties(self):
-
-        self.ship.x += self.ship.x_velocity
-        self.ship.y += self.ship.y_velocity
-        self.ship.rotation += self.ship.rotational_velocity
-
-        # Prevent the speed of the ship from exceeding its maximum.
-        if self.ship.x_velocity**2 + self.ship.y_velocity**2 > self.ship.max_speed**2:
-            self.ship.x_velocity *= 0.99
-            self.ship.y_velocity *= 0.99
-
-        # Prevent the rotational speed of the ship from exceeding its maximum.
-        if fabs(self.ship.rotational_velocity) > self.ship.max_rotational_speed:
-            self.ship.rotational_velocity *= 0.99
+        self.target.position = new.center
 
 
 class Game(Layer):
@@ -104,62 +74,29 @@ class Game(Layer):
         self.scrolling_manager = ScrollingManager()
         self.add(self.scrolling_manager)
 
-        #add scrolling manager to layer1 of the map
+        #add scrolling manager to  the map
         self.background = Background()
         self.scrolling_manager.add(self.background.layer1)
         self.scrolling_manager.add(self.background.layer2)
         self.scrolling_manager.add(self.background.layer3)
         self.scrolling_manager.add(self.background.layer4)
 
-        self.player_ship_layer = Player_Ship_Layer()
+
+        global keyboard
+
+        # Attach a KeyStateHandler to the keyboard object.
+        keyboard = key.KeyStateHandler()
+        director.window.push_handlers(keyboard)
+
+        mapcollider = mapcolliders.TmxObjectMapCollider()
+        mapcollider.on_bump_handler = mapcollider.on_bump_bounce
+        collision_handler = mapcolliders.make_collision_handler(mapcollider, self.background.colliders)
+
+        self.player_ship_layer = Player_Layer(collision_handler)
         # This is just to create a reference shortcut for later, repeated use.
-        self.player_ship = self.player_ship_layer.ship
+       # self.player_ship = self.player_ship_layer.ship
 
         self.scrolling_manager.add(self.player_ship_layer)
-
-
-        self.keys = {
-            key.symbol_string(key.A): False,
-            key.symbol_string(key.D): False,
-            key.symbol_string(key.W): False,
-            key.symbol_string(key.S): False,
-            key.symbol_string(key.SPACE): False,
-            key.symbol_string(key.R): False
-        }
-
-
-
-        # See the definition of the Game class's update function for an explanation.
-        self.dt_accumulator = 0
-        self.update_period = 1/60
-        self.schedule(self.update)
-
-
-    def on_key_press(self, k, modifiers):
-        self.keys[key.symbol_string(k)] = True
-
-    def on_key_release(self, k, modifiers):
-        self.keys[key.symbol_string(k)] = False
-
-
-    def update(self, dt):
-
-        '''
-        To keep a fairly regular update rate, the time since the last update is accumulated. When the accumulator is
-        greater than the update period, an update is invoked, and then the accumulator is reduced by the update period.
-        There may be some residual time left in the accumulator after this, which may build up over repeated iterations.
-        If the accumulator holds a value equal to or greater than twice the update period, multiple update steps are
-        invoked on the same update until the accumulator is reduced to a value less than the update period.
-        '''
-        self.dt_accumulator += dt
-        while self.dt_accumulator > self.update_period:
-
-            self.player_ship_layer.handle_key_input(self.keys)
-            self.player_ship_layer.update_ship_spacial_properties()
-
-            self.scrolling_manager.set_focus(self.player_ship.x, self.player_ship.y)
-
-            self.dt_accumulator -= self.update_period
 
 
 
